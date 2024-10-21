ce5a4f80cb8a8197a9d9562a7a702fca6d03ac78
Dave Cameron <83275112+DavidCameron33@users.noreply.github.com>
2024-10-21 13:46:55 +0100

    Models etc. moved from Tests to Checkout project; Tested OK

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -2,6 +2,36 @@
 {
     public class Checkout
     {
+        List<StoreItem> storeItems;
+        public Checkout(StoreItems _storeItems)
+        {
+            storeItems = _storeItems.StockItems;
+        }
 
+        Dictionary<string, int> cartItems = new Dictionary<string, int>();
+
+        public int ScanInNoSpecials(IEnumerable<string> shoppingCart)
+        {
+            int subTotal = 0;
+
+            foreach (string cartItem in shoppingCart)
+            {
+                if (!cartItems.ContainsKey(cartItem))
+                {
+                    cartItems.Add(cartItem, 0);
+                }
+                cartItems[cartItem]++;
+            }
+
+            foreach (var cartItem in cartItems)
+            {
+                var units = cartItem.Value;
+                StoreItem storeItem = storeItems.Where(i => i.SKU == cartItem.Key.ToString()).First();
+                var unitPrice = storeItem.UnitPrice;
+                subTotal += (units * unitPrice);
+            }
+
+            return subTotal;
+        }
     }
 }
---
+++ Checkout/StoreItems.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout
+{
+    public class StoreItems
+    {
+        public List<StoreItem> StockItems { get; set; }
+        public StoreItems()
+        {
+            StockItems = new List<StoreItem>();
+        }
+    }
+    public class StoreItem
+    {
+        public string SKU { get; set; }
+        public int UnitPrice { get; set; }
+        public SpecialPrice SpecialPrice { get; set; }
+
+        public StoreItem(string sku, int unitPrice, SpecialPrice specialPrice = null)
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+            SpecialPrice = specialPrice;
+        }
+    }
+    public class SpecialPrice
+    {
+        public int Qty { get; set; }
+        public int Price { get; set; }
+
+        public SpecialPrice(int qty, int price)
+        {
+            Qty = qty;
+            Price = price;
+        }
+    }
+}
