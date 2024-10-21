783a27cf781d36f821fd929317189ffce6568139
Dave Cameron <83275112+DavidCameron33@users.noreply.github.com>
2024-10-21 14:15:33 +0100

    `ShouldReturnDiscountedItemsPrice` changed from Fact to Theory with InlineData added;
    Tested OK

--- Checkout.Tests/CheckoutTests.cs
+++ Checkout.Tests/CheckoutTests.cs
@@ -33,8 +33,9 @@ namespace Checkout
             Assert.NotNull(shoppingCartTotal);
             Assert.Equal(50, shoppingCartTotal);
         }
+
         [Fact]
-        public void ShouldReturnDiscountedItemsPrice()
+        public void ShouldReturnDiscountedItemsPrice1()
         {
             //	Arrange
             IEnumerable<string> shoppingCart = new[] { "A" };
@@ -45,6 +46,32 @@ namespace Checkout
             //	Assert
             Assert.NotNull(shoppingCartTotal);
             Assert.Equal(50, shoppingCartTotal);
+        }
+
+        /// 1xA=50
+        [InlineData(50, new[] { "A" })]
+
+        /// 3xA=130
+        [InlineData(130, new[] { "A", "A", "A" })]
+
+        /// 2xB=45; 1xA=50: 45+50=95
+        [InlineData(95, new[] { "B", "A", "B" })]
+
+        /// 1xA=50; 1xB=30; 1xC=20; 1xD=15: 50+30+20+15=115
+        [InlineData(115, new[] { "A", "B", "C", "D" })]
+
+        /// 4xA=180 (3xA=130; 1xA=50: 130+50=180); 4xB=90 (2xB=45; 2xB=45: 45+45=90) 2xC=40; 1xD=15: 180+90+40+15=325
+        [InlineData(325, new[] { "A", "B","A", "C", "B", "D", "B", "C", "A", "B", "A" })]    
+        [Theory]
+        public void ShouldReturnDiscountedItemsPrice(int total, IEnumerable<string> shoppingCart)
+        {
+            //	Arrange
+            //	Act
+            int shoppingCartTotal = _processor.ScanInWithSpecials(shoppingCart);
+
+            //	Assert
+            Assert.NotNull(shoppingCartTotal);
+            Assert.Equal(total, shoppingCartTotal);
         }
     }
 }
